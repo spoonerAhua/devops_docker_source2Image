3d57b27c1ebc6529e3d5052154df034a1cf072f0
spooner <67099059@qq.com>
2019-12-25 00:00:40 +0800

    init
    init

--- source2image.sh
+++ source2image.sh
@@ -1,5 +1,42 @@
-rm -rf srcFromGit
-git clone https://github.com/spoonerAhua/devops_docker_dockerfile2Image_nodejs.git srcFromGit
-docker build -t devops_docker_d2i:v0.1 srcFromGit
-rm -rf srcFromGit
+#!/bin/bash
+function cleanEnv()
+{
+  rm -f srcFromGit
+  rm -f dockerfilesFromGit
+}
+
+function printHelp()
+{
+  echo The programe need two parameters. language type and git link
+  echo language type: php or nodejs
+  echo Example:
+  echo   source2image.bat nodejs https://github.com/spoonerAhua/devops_docker_dockerfile2Image_nodejs.git
+}
+
+srcLink=$3
+imageName=$2
+languageType=$1
+
+if [ "$srcLink" = "" ]; then
+  printHelp
+  exit 1
+fi
+
+cleanEnv
+
+git clone https://github.com/spoonerAhua/devops_docker_source2Image.git dockerfilesFromGit
+git clone %srcLink% srcFromGit
+
+if [ $languageType = "php" ]; then
+  echo "-->build php"
+elif [ $languageType = "nodejs" ]; then
+  echo "-->build nodejs"
+  copy dockerfilesFromGit\Dockerfile_nodejs srcFromGit\Dockerfile
+  docker build -t %imageName% srcFromGit
+  goto clear
+fi
+
+echo docker build -t %imageName% srcFromGi
+
+cleanEnv
 
